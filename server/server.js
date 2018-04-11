4748f01dc758c9e97fa72bd2faca6f3d51fed0b5
Ayoub <ghozziayoub@gmail.com>
2018-04-11 18:13:10 +0100

    setup to heroku 2

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
